22dff3e8983d90ef8d07f8ced5f7ea85a48a187b
Dimitar <Dimitarit1627@gmail.com>
2023-08-08 10:48:59 +0300

    Add classes and packages with commands

--- src/com/management/oop/project/models/contracts/Commentable.java
+++ src/com/management/oop/project/models/contracts/Commentable.java
@@ -6,6 +6,4 @@ public interface Commentable {
 
     List<Comment> getComments();
 
-
-
 }
--- src/com/management/oop/project/models/tasks/BugImpl.java
+++ src/com/management/oop/project/models/tasks/BugImpl.java
@@ -2,6 +2,22 @@ package com.management.oop.project.models.tasks;
 
 import com.management.oop.project.models.contracts.Bug;
 
-public class BugImpl extends TaskBase implements Bug {
+import java.util.List;
 
+public class BugImpl extends TaskBase implements Bug {
+    private List<String>steps;
+
+    public BugImpl(String title, String description) {
+        super(title, description);
+    }
+
+    @Override
+    public void addComment() {
+
+    }
+
+    @Override
+    public void removeComment() {
+
+    }
 }
