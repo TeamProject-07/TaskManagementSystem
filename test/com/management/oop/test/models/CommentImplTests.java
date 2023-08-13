0fb68c77880476ecb685cc5bcb8005fc5b392f7b
Dimitar <Dimitarit1627@gmail.com>
2023-08-13 15:41:31 +0300

    Implement BoardImplTests and CommentImplTests

--- test/com/management/oop/test/models/CommentImplTests.java
+++ test/com/management/oop/test/models/CommentImplTests.java
@@ -1,4 +1,26 @@
 package com.management.oop.test.models;
 
+import com.management.oop.project.models.CommentImpl;
+import com.management.oop.project.models.enums.FeedbackStatusEnum;
+import com.management.oop.project.models.tasks.FeedbackImpl;
+import com.management.oop.test.utils.TaskBaseConstants;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CommentImplTests {
+    @Test
+    public void constructor_Should_CreateNewComment_When_ParametersAreCorrect() {
+        // Arrange, Act
+        CommentImpl comment = initializeTestComment();
+
+        // Assert
+        Assertions.assertEquals("message", comment.getMessage());
+        Assertions.assertEquals("author", comment.getAuthor());
+    }
+
+    public static CommentImpl initializeTestComment() {
+        return new CommentImpl(
+                "message",
+                "author");
+    }
 }
