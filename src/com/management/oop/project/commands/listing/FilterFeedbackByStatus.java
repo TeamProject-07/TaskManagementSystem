6c49f16c6e795b242e16cc808b9898ab35269616
Dora <dimitrov.dora@gmail.com>
2023-08-17 11:26:59 +0200

    FilterFeedbackByStatus and SortFeedback

--- src/com/management/oop/project/commands/listing/FilterFeedbackByStatus.java
+++ src/com/management/oop/project/commands/listing/FilterFeedbackByStatus.java
@@ -1,5 +1,35 @@
 package com.management.oop.project.commands.listing;
 
-public class FilterFeedbackByStatus {
+import com.management.oop.project.commands.contracts.Command;
+import com.management.oop.project.core.contracts.TaskManagementSystemRepository;
+import com.management.oop.project.models.contracts.Feedback;
+import com.management.oop.project.utils.ValidationHelpers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterFeedbackByStatus implements Command {
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 1;
+    TaskManagementSystemRepository taskManagementSystemRepository;
+    private List<Feedback> feedbacks;
+
+    public FilterFeedbackByStatus(TaskManagementSystemRepository taskManagementSystemRepository, List<Feedback> feedbacks) {
+        this.taskManagementSystemRepository = taskManagementSystemRepository;
+        this.feedbacks = feedbacks;
+    }
+
+    @Override
+    public String execute(List<String> parameters) {
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+
+        String parameter = parameters.get(0);
+        return filterFeedback(parameter).toString();
+    }
+
+    private List<Feedback> filterFeedback(String status){
+        return feedbacks
+                .stream()
+                .filter(feedback -> feedback.getStatus().equals(status))
+                .collect(Collectors.toList());
+    }
 }
--- src/com/management/oop/project/commands/listing/SortFeedback.java
+++ src/com/management/oop/project/commands/listing/SortFeedback.java
@@ -1,4 +1,43 @@
 package com.management.oop.project.commands.listing;
 
-public class SortFeedback {
+import com.management.oop.project.commands.contracts.Command;
+import com.management.oop.project.core.contracts.TaskManagementSystemRepository;
+import com.management.oop.project.models.contracts.Feedback;
+import com.management.oop.project.utils.ValidationHelpers;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SortFeedback implements Command {
+
+    TaskManagementSystemRepository taskManagementSystemRepository;
+    private List<Feedback> feedbacks;
+
+    public SortFeedback(TaskManagementSystemRepository taskManagementSystemRepository, List<Feedback> feedbacks) {
+        this.taskManagementSystemRepository = taskManagementSystemRepository;
+        this.feedbacks = feedbacks;
+    }
+
+    @Override
+    public String execute(List<String> parameters) {
+        return sortFeedback();
+    }
+
+    private String sortFeedback(){
+//        StringBuilder list = new StringBuilder();
+//        feedbacks.sort(Comparator.comparing(Feedback::getTitle)
+//                .thenComparing(Feedback::getRating));
+//        for (Feedback feedback : feedbacks) {
+//            list.append(feedback.getTitle()).append(" ")
+//                    .append(feedback.getRating())
+//                    .append(System.lineSeparator());
+//        }
+//        return list.toString();
+        return feedbacks
+                .stream()
+                .sorted(Comparator.comparing(Feedback :: getTitle).thenComparing(Feedback::getRating))
+                .map(feedback -> feedback.getTitle() + " " + feedback.getRating())
+                .collect(Collectors.toList()).toString();
+    }
 }
