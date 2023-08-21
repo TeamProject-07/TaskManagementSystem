1a215f875c514f0879ea1a0311103ccc672655a2
KatyaStepanova <kateryna.stepanova99@gmail.com>
2023-08-21 10:59:03 +0300

    Update SortStoryTest

--- test/com/management/oop/test/commands/listing/SortAllTasksByTitleTest.java
+++ test/com/management/oop/test/commands/listing/SortAllTasksByTitleTest.java
@@ -1,38 +1,31 @@
 package com.management.oop.test.commands.listing;
 
+import com.management.oop.project.commands.contracts.Command;
 import com.management.oop.project.commands.listing.SortAllTasksByTitle;
 import com.management.oop.project.core.TaskManagementSystemRepositoryImpl;
 import com.management.oop.project.core.contracts.TaskManagementSystemRepository;
-import com.management.oop.project.models.BoardImpl;
-import com.management.oop.project.models.TeamImpl;
 import com.management.oop.project.models.contracts.Board;
-import com.management.oop.project.models.contracts.Feedback;
 import com.management.oop.project.models.contracts.Task;
 import com.management.oop.project.models.contracts.Team;
-import com.management.oop.project.models.enums.*;
-import com.management.oop.project.models.tasks.BugImpl;
-import com.management.oop.project.models.tasks.FeedbackImpl;
-import com.management.oop.project.models.tasks.StoryImpl;
+import com.management.oop.test.utils.TestUtilities;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class SortAllTasksByTitleTest {
-    private TaskManagementSystemRepository repository;
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 0;
+    private TaskManagementSystemRepository taskManagementSystemRepository;
 
-    private SortAllTasksByTitle sortAllTasksByTitle;
+    private Command sortAllTasksByTitle;
 
     private Task task;
 
     private List<Task> tasks;
 
-    private List<String> params;
-
-    private List<String> sortedTasks;
+    private List<String> parameters;
 
     private Board board;
 
@@ -41,45 +34,18 @@ public class SortAllTasksByTitleTest {
 
     @BeforeEach
     public void before() {
-        repository = new TaskManagementSystemRepositoryImpl();
-        sortAllTasksByTitle = new SortAllTasksByTitle(repository);
-        List<String> params = new ArrayList<>();
-       // Team team = new TeamImpl("teamName");
-       // Board board = new BoardImpl("boardName");
-        repository.createTeam("teamName");
-        repository.createBoard("boardName", "teamName");
-        repository.createFeedback("boardName","Zzzzzzzzzzzz", "description", 25, FeedbackStatusEnum.NEW);
-        repository.createBug("boardName", "VerySeriousProblem", "description", List.of("step1", "step2"), PriorityEnum.HIGH, BugSeverityEnum.CRITICAL );
-        repository.createStory("boardName",  "Aaaaaaaaaaaaaa", "description", PriorityEnum.MEDIUM, StorySizeEnum.LARGE, StoryStatusEnum.IN_PROGRESS);
-
-     //  Task task1 = new FeedbackImpl(1, "Zzzzzzzzzzzz", "description", 25, FeedbackStatusEnum.NEW);
-     //  Task task2 = new BugImpl(2, "VerySeriousProblem", "description", List.of("step1", "step2"), PriorityEnum.HIGH, BugSeverityEnum.CRITICAL);
-     //  Task task3 = new StoryImpl(3, "Aaaaaaaaaaaaaa", "description", PriorityEnum.MEDIUM, StorySizeEnum.LARGE, StoryStatusEnum.IN_PROGRESS);
-
+        parameters = new ArrayList<>();
+        taskManagementSystemRepository = new TaskManagementSystemRepositoryImpl();
+        sortAllTasksByTitle = new SortAllTasksByTitle(taskManagementSystemRepository);
 
     }
 
     @Test
-    public void should_SortTasks_WhenArguments_AreValid() {
-      //  List<Task> tasks = new ArrayList<>();
-      //List<String> params = new ArrayList<>();
-      //  Task task1 = new FeedbackImpl(1, "Zzzzzzzzzzzz", "description", 25, FeedbackStatusEnum.NEW);
-      //  Task task2 = new BugImpl(2, "VerySeriousProblem", "description", List.of("step1", "step2"), PriorityEnum.HIGH, BugSeverityEnum.CRITICAL);
-      //  Task task3 = new StoryImpl(3, "Aaaaaaaaaaaaaa", "description", PriorityEnum.MEDIUM, StorySizeEnum.LARGE, StoryStatusEnum.IN_PROGRESS);
-//
-      //  tasks.add(task1);
-      //  tasks.add(task2);
-      //  tasks.add(task3);
+    public void should_ThrowException_When_ArgumentCountInvalid() {
+        // Arrange
+        parameters = TestUtilities.getList(EXPECTED_NUMBER_OF_ARGUMENTS + 1);
 
-       String result = sortAllTasksByTitle.execute(params);
-
-     //   String[] resultArray = result.split(",");
-     //   List<String> titlesList = Arrays.asList(resultArray);
-     //   Assertions.assertEquals(3, tasks.size());
-      // Assertions.assertEquals("Aaaaaaaaaaaaaa", titlesList.get(1));
-      // Assertions.assertEquals("VerySeriousProblem", titlesList.get(2));
-      // Assertions.assertEquals("Zzzzzzzzzzzz", titlesList.get(3));
-
-        Assertions.assertEquals("[Aaaaaaaaaaaaaa, VerySeriousProblem, Zzzzzzzzzzzz]", result);
+        // Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> sortAllTasksByTitle.execute(parameters));
     }
 }
---
+++ test/com/management/oop/test/commands/listing/SortStoryTest.java
@@ -0,0 +1,44 @@
+package com.management.oop.test.commands.listing;
+
+import com.management.oop.project.commands.contracts.Command;
+import com.management.oop.project.commands.listing.SortStory;
+import com.management.oop.project.core.TaskManagementSystemRepositoryImpl;
+import com.management.oop.project.core.contracts.TaskManagementSystemRepository;
+import com.management.oop.project.models.enums.PriorityEnum;
+import com.management.oop.project.models.enums.StorySizeEnum;
+import com.management.oop.project.models.enums.StoryStatusEnum;
+import com.management.oop.test.utils.TaskBaseConstants;
+import com.management.oop.test.utils.TestUtilities;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortStoryTest {
+
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 0;
+    private List<String> parameters;
+    private TaskManagementSystemRepository taskManagementSystemRepository;
+    private Command sortStory;
+
+    @BeforeEach
+    public void before(){
+        parameters = new ArrayList<>();
+        taskManagementSystemRepository = new TaskManagementSystemRepositoryImpl();
+        sortStory = new SortStory(taskManagementSystemRepository);
+        taskManagementSystemRepository.createTeam(TaskBaseConstants.VALID_TEAM_NAME);
+        taskManagementSystemRepository.createBoard(TaskBaseConstants.VALID_BOARD_NAME, TaskBaseConstants.VALID_TEAM_NAME);
+        taskManagementSystemRepository.createStory(TaskBaseConstants.VALID_BOARD_NAME, TaskBaseConstants.VALID_TITLE,
+                TaskBaseConstants.VALID_DESCRIPTION , PriorityEnum.MEDIUM, StorySizeEnum.LARGE, StoryStatusEnum.IN_PROGRESS);
+    }
+    @Test
+    public void should_ThrowException_When_ArgumentCountInvalid() {
+        // Arrange
+        parameters = TestUtilities.getList(EXPECTED_NUMBER_OF_ARGUMENTS + 1);
+
+        // Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> sortStory.execute(parameters));
+    }
+}
