fd6fb67574a2ad6eb62dee0ccb1cb4410e52ced3
KatyaStepanova <kateryna.stepanova99@gmail.com>
2023-08-21 14:20:38 +0300

    FilterByStatusAndAssignee

--- src/com/management/oop/project/commands/listing/FilterBugByStatusAndAssignee.java
+++ src/com/management/oop/project/commands/listing/FilterBugByStatusAndAssignee.java
@@ -1,4 +1,44 @@
 package com.management.oop.project.commands.listing;
 
-public class FilterBugByStatusAndAssignee {
+import com.management.oop.project.commands.contracts.Command;
+import com.management.oop.project.core.contracts.TaskManagementSystemRepository;
+import com.management.oop.project.models.contracts.Assignable;
+import com.management.oop.project.models.contracts.Person;
+import com.management.oop.project.models.enums.BugStatusEnum;
+import com.management.oop.project.utils.ListingHelpers;
+import com.management.oop.project.utils.ParsingHelpers;
+import com.management.oop.project.utils.ValidationHelpers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterBugByStatusAndAssignee implements Command {
+
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+
+    private final TaskManagementSystemRepository taskManagementSystemRepository;
+
+    public FilterBugByStatusAndAssignee(TaskManagementSystemRepository taskManagementSystemRepository) {
+        this.taskManagementSystemRepository = taskManagementSystemRepository;
+    }
+
+    @Override
+    public String execute(List<String> parameters) {
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+        BugStatusEnum statusEnum = ParsingHelpers.tryParseEnum(parameters.get(1), BugStatusEnum.class);
+        String personName = parameters.get(0);
+        Person person = taskManagementSystemRepository.findPersonByName(personName);
+        return ListingHelpers.getAsString(filterBugByAssigneeAndStatus(person, statusEnum));
+    }
+
+
+    private List<Assignable> filterBugByAssigneeAndStatus(Person assignee, BugStatusEnum statusEnum) {
+        return taskManagementSystemRepository.getAllBugs()
+                .stream()
+                .filter(bug -> bug.getAssignee().equals(assignee))
+                .filter(bug -> bug.getStatus().equals(statusEnum))
+                .collect(Collectors.toList());
+
+
+    }
 }
--- src/com/management/oop/project/commands/listing/FilterStoryByStatusAndAssignee.java
+++ src/com/management/oop/project/commands/listing/FilterStoryByStatusAndAssignee.java
@@ -1,4 +1,45 @@
 package com.management.oop.project.commands.listing;
 
-public class FilterStoryByStatusAndAssignee {
+import com.management.oop.project.commands.contracts.Command;
+import com.management.oop.project.core.contracts.TaskManagementSystemRepository;
+import com.management.oop.project.models.contracts.Assignable;
+import com.management.oop.project.models.contracts.Person;
+import com.management.oop.project.models.enums.BugStatusEnum;
+import com.management.oop.project.models.enums.StoryStatusEnum;
+import com.management.oop.project.utils.ListingHelpers;
+import com.management.oop.project.utils.ParsingHelpers;
+import com.management.oop.project.utils.ValidationHelpers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterStoryByStatusAndAssignee implements Command {
+
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+
+    private final TaskManagementSystemRepository taskManagementSystemRepository;
+
+    public FilterStoryByStatusAndAssignee(TaskManagementSystemRepository taskManagementSystemRepository) {
+        this.taskManagementSystemRepository = taskManagementSystemRepository;
+    }
+
+
+    @Override
+    public String execute(List<String> parameters) {
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+        String personName = parameters.get(0);
+        Person person = taskManagementSystemRepository.findPersonByName(personName);
+        StoryStatusEnum statusEnum = ParsingHelpers.tryParseEnum(parameters.get(1), StoryStatusEnum.class);
+        return ListingHelpers.getAsString(filterStoryByAssigneeAndStatus(person, statusEnum));
+    }
+
+    private List<Assignable> filterStoryByAssigneeAndStatus(Person assignee, StoryStatusEnum statusEnum) {
+        return taskManagementSystemRepository.getAllStories()
+                .stream()
+                .filter(story -> story.getAssignee().equals(assignee))
+                .filter(story -> story.getStoryStatusEnum().equals(statusEnum))
+                .collect(Collectors.toList());
+
+
+    }
 }
