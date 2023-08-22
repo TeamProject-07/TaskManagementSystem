899ff54888b75729291d72ec0f3323bb96bfeb6e
Dora <dimitrov.dora@gmail.com>
2023-08-22 19:00:17 +0200

    Update SortTaskWithAssigneeByTitleTest

--- test/com/management/oop/test/commands/listing/SortTaskWithAssigneeByTitleTest.java
+++ test/com/management/oop/test/commands/listing/SortTaskWithAssigneeByTitleTest.java
@@ -4,6 +4,14 @@ import com.management.oop.project.commands.contracts.Command;
 import com.management.oop.project.commands.listing.SortTasksWithAssigneeByTitle;
 import com.management.oop.project.core.TaskManagementSystemRepositoryImpl;
 import com.management.oop.project.core.contracts.TaskManagementSystemRepository;
+import com.management.oop.project.models.contracts.Board;
+import com.management.oop.project.models.contracts.Bug;
+import com.management.oop.project.models.contracts.Story;
+import com.management.oop.project.models.enums.BugSeverityEnum;
+import com.management.oop.project.models.enums.PriorityEnum;
+import com.management.oop.project.models.enums.StorySizeEnum;
+import com.management.oop.project.models.enums.StoryStatusEnum;
+import com.management.oop.test.utils.TaskBaseConstants;
 import com.management.oop.test.utils.TestUtilities;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,12 +25,35 @@ public class SortTaskWithAssigneeByTitleTest {
     private List<String> parameters;
     private TaskManagementSystemRepository taskManagementSystemRepository;
     private Command sortTaskWithAssignee;
+    private Board board;
+
+    private Story story;
+    private Bug bug;
 
     @BeforeEach
     public void before() {
         taskManagementSystemRepository = new TaskManagementSystemRepositoryImpl();
         parameters = new ArrayList<>();
         sortTaskWithAssignee = new SortTasksWithAssigneeByTitle(taskManagementSystemRepository);
+        taskManagementSystemRepository.createTeam(TaskBaseConstants.VALID_TEAM_NAME);
+
+        this.board=taskManagementSystemRepository.createBoard(
+                TaskBaseConstants.VALID_BOARD_NAME,
+                TaskBaseConstants.VALID_TEAM_NAME);
+        this.bug = taskManagementSystemRepository.createBug(
+                board.getName(),
+                TaskBaseConstants.VALID_TITLE,
+                TaskBaseConstants.VALID_DESCRIPTION,
+                TaskBaseConstants.STEPS,
+                PriorityEnum.HIGH,
+                BugSeverityEnum.CRITICAL);
+        this.story = taskManagementSystemRepository.createStory(
+                board.getName(),
+                TaskBaseConstants.VALID_TITLE_1,
+                TaskBaseConstants.VALID_DESCRIPTION,
+                PriorityEnum.HIGH,
+                StorySizeEnum.LARGE,
+                StoryStatusEnum.DONE);
     }
 
     @Test
@@ -31,6 +62,22 @@ public class SortTaskWithAssigneeByTitleTest {
         parameters = TestUtilities.getList(EXPECTED_NUMBER_OF_ARGUMENTS + 1);
 
         // Act, Assert
-        Assertions.assertThrows(IllegalArgumentException.class, () -> sortTaskWithAssignee.execute(parameters));
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> sortTaskWithAssignee.execute(parameters));
+    }
+
+    @Test
+    public void shouldSortTask_When_ExecuteCommand(){
+        // Arrange
+        List<String> parameters = new ArrayList<>();
+
+        // Act
+        List<String> tasks = new ArrayList<>();
+        tasks.add(bug.getTitle());
+        tasks.add(story.getTitle());
+
+        // Assert
+        Assertions.assertEquals(tasks.toString(),
+                sortTaskWithAssignee.execute(parameters));
     }
 }
